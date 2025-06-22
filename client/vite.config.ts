d3a1ddbc903cc1c6cfe22154ac31eee2eadfbb80
cdy <dragon5319@gmail.com>
2025-06-22 20:37:03 +0900

    reload modify

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  base: '/', // 중요: 경로가 /로 지정되어야 새로고침 대응 가능
 });
