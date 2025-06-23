6f0e6970217ab9f3f875bd8ddeeb2b7ed25ef0ba
대용 <devxcdy@gmail.com>
2025-06-23 16:23:45 +0900

    또 수정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <DndProvider backend={HTML5Backend}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </DndProvider>
   </React.StrictMode>
 );
