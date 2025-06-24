805dd75d2454923e471665264393a0237fe16156
대용 <devxcdy@gmail.com>
2025-06-24 13:01:39 +0900

    checked 삭제

--- client/src/pages/admin/AdminCompanyFormPage.tsx
+++ client/src/pages/admin/AdminCompanyFormPage.tsx
@@ -42,10 +42,10 @@ const AdminCompanyFormPage: React.FC = () => {
   }, [id]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value, type, checked } = e.target;
+    const { name, value, type } = e.target;
     setForm((prev) => ({
       ...prev,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === 'checkbox' ? (e.target as HTMLInputElement).checked : value,
     }));
   };
 
